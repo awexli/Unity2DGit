7e38e7aa88a6a000e021b93e676c4759e601a0f4
awexli <li_alex1415@hotmail.com>
2019-01-08 20:50:46 -0800

    test

---
+++ Assets/Alex's Folder/Alex's Scripts/CamClamp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// creates xMax, xMin, yMax, yMin variables for camera on specific levels.
+// prevents camera from moving past these variables
+
+[DisallowMultipleComponent]
+public class CamClamp : MonoBehaviour {
+	[SerializeField]Transform target;
+	[SerializeField]float xMin = -1;
+	[SerializeField]float xMax = 1;
+	[SerializeField]float yMin = -1;
+	[SerializeField]float yMax = 1;
+	
+	
+	Transform t;
+	
+	void Awake ()
+	{
+		t = transform;
+	}
+	
+	void LateUpdate()
+	{
+		float x = Mathf.Clamp(target.position.x, xMin, xMax);
+		float y = Mathf.Clamp(target.position.y, yMin, yMax);
+		t.position = new Vector3(x, y, t.position.z);
+	}
+	
+}
