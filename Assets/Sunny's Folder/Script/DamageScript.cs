0839f5f7f54f7a174e093ea4f3b4bbea08d3d12e
Sunny Wong <swong6@mail.sfsu.edu>
2019-01-08 21:25:18 -0800

    Sunny added most of his prefabs

---
+++ Assets/Sunny's Folder/Script/DamageScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        GameControlScript.health -= 1;
+        if (col.transform.CompareTag("Player"))
+            col.transform.position = spawnPoint.position;
+    }
+
+}
