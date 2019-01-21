e358fb20363e6b91f8a591881370f873bb4e4bf0
awexli <li_alex1415@hotmail.com>
2019-01-20 21:04:06 -0800

    Scene1 upgrade

---
+++ Assets/Alex's Folder/Alex's Scripts/PlayerPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatform : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.transform.tag == "Moving Platform")
+		{
+			transform.parent = other.transform;
+		}
+	}
+	
+	 void OnCollisionExit2D(Collision2D other)
+	{
+		if (other.transform.tag == "Moving Platform")
+		{
+			transform.parent = null;
+		}
+	}
+}
