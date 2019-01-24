1b7665799fc34852adb7c562b908c638722cb8ee
Dakimakuras <jdvaldez@mail.sfsu.edu>
2019-01-23 21:16:28 -0800

    Created pause menu for LevelBlank which can be now used throughout the game to create pause menu for everyone. Fixed and made some noise for mainmenu so when you quit application noise plays.

--- Assets/Scripts (General)/DamageScript.cs
+++ Assets/Scripts (General)/DamageScript.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class DamageScript : MonoBehaviour
 {
     [SerializeField] Transform spawnPoint;
-    public AudioSource restartSound;
     void OnCollisionEnter2D(Collision2D col)
     {
         GameControlScript.health -= 1;
         if (col.transform.CompareTag("Player"))
-        	restartSound.Play();
             col.transform.position = spawnPoint.position;
     }
 }
